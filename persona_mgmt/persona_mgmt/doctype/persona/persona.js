55d10b554e11db00916fdbed82213a83eed02a46
Faisal Zahoor <faisal-zahoor>
2025-09-13 11:29:17 +0500

    feat: Add Persona Management DocTypes and related files

---
+++ persona_mgmt/persona_mgmt/doctype/persona/persona.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Faisal Zahoor and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Persona", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ persona_mgmt/persona_mgmt/doctype/persona/test_persona.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Faisal Zahoor and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPersona(FrappeTestCase):
+	pass
